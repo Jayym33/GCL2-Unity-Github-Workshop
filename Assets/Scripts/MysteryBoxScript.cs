497e4a79c627fb2ca33edc8dee3184a29c79c208
Jayym33 <hyxjamie@gmail.com>
2026-04-29 11:42:29 +0800

    [Jamie} Added mystery box + mystery box script

---
+++ Assets/Scripts/MysteryBoxScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MysteryBoxScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Mystery Box spawned!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
